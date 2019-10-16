5ff0d33970048e85044a6db492b1bf423ac77c96
huchengbei <huqiao@imhuqiao.com>
2019-10-16 10:59:03 +0800

    add scroll up

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -6,5 +6,15 @@
       function(){$(this).dropdown('toggle')});
   $(".dropdown-menu").on('mouseout',
       function(){$(this).parent().children('.dropdown-toggle').dropdown('toggle')});
+  $.scrollUp({
+    scrollName: "scrollUp",
+    topDistance: "300",
+    topSpeed: 300,
+    animation: "fade",
+    animationInSpeed: 200,
+    animationOutSpeed: 200,
+    scrollText: '<div class="mt-2"><i class="fa fa-arrow-up"></i></div>',
+    activeOverlay: !1
+  });
 
 })(jQuery);
