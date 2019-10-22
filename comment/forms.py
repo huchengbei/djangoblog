0a1110160fcd84a1722a8504d47d0396da3370b4
huchengbei <huqiao@imhuqiao.com>
2019-10-22 10:36:20 +0800

    add anti_spam test

--- comment/forms.py
+++ comment/forms.py
@@ -4,6 +4,7 @@ from comment.models import Comment
 
 
 class CommentForm(forms.ModelForm):
+    anti_spam = forms.CharField()
 
     class Meta:
         model = Comment
@@ -11,6 +12,7 @@ class CommentForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['anti_spam'].widget = forms.HiddenInput()
         self.fields['article'].widget = forms.HiddenInput()
         self.fields['parent'].widget = forms.HiddenInput()
         self.fields['user'].widget = forms.HiddenInput()
@@ -19,3 +21,8 @@ class CommentForm(forms.ModelForm):
         self.fields['website'].widget.attrs['placeholder'] = "网站"
         self.fields['content'].widget.attrs['placeholder'] = "一字千金"
         self.fields['content'].widget.attrs['rows'] = "5"
+
+    def clean_anti_spam(self):
+        anti_spam = self.cleaned_data.get('anti_spam')
+        if anti_spam:
+            self.add_error('anti_spam', 'spam comment')
